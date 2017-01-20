6d04e9f72c7001653c390bd5c0a8b4d8706f3e59
Aaron Pennington <aaron.l.pennington@gmail.com>
2017-01-20 14:43:28 -0700

    logging io object a different way take 4

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ io.sockets.on('connection', function(socket) {
     log('Received request to create or join room ' + room);
 
     var numClients = io.sockets.sockets;
-    console.log('io object: '.yellow, io.sockets);
+    console.log('io.sockets object: '.yellow, io.sockets.rooms);
     log('Room ' + room + ' now has ' + numClients + ' client(s)');
 
     if (numClients <= 1) {
