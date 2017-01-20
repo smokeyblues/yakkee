70ea6f2ed8c0df16f825431ec6019f4812e0b8c0
Aaron Pennington <aaron.l.pennington@gmail.com>
2017-01-20 16:03:55 -0700

    added socket service

---
+++ public/js/services/service.socket.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Create the Socket.io wrapper service
+angular.module('Yakkee').service('Socket', ['Auth', '$state', '$timeout',
+  function (Auth, $state, $timeout) {
+    // Connect to Socket.io server
+    this.connect = function () {
+      // Connect only when authenticated
+      if (Auth.user) {
+        this.socket = io();
+        // or is this where socket should notify server of online status -- my note
+      }
+    };
+    this.connect();
+
+    // Wrap the Socket.io 'on' method
+    this.on = function (eventName, callback) {
+      if (this.socket) {
+        this.socket.on(eventName, function (data) {
+          $timeout(function () {
+            callback(data);
+          });
+        });
+      }
+    };
+
+    // Wrap the Socket.io 'emit' method
+    this.emit = function (eventName, data) {
+      if (this.socket) {
+        this.socket.emit(eventName, data);
+      }
+    };
+
+    // Wrap the Socket.io 'removeListener' method
+    this.removeListener = function (eventName) {
+      if (this.socket) {
+        this.socket.removeListener(eventName);
+      }
+    };
+  }
+]);
