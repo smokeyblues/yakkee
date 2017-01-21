253a7b93e174a9778cc84adea50eb45e386374e5
Aaron Pennington <aaron.l.pennington@gmail.com>
2017-01-21 14:40:41 -0700

    making changes to controller.register for file upload

--- public/js/controllers/controller.register.js
+++ public/js/controllers/controller.register.js
@@ -1,15 +1,15 @@
 angular.module('Yakkee')
   .controller('registerController', registerController);
 
-  registerController.$inject = ['$http', 'Auth', '$location'];
+  registerController.$inject = ['$http', 'Auth', '$location', 'Upload'];
 
-  function registerController($http, Auth, $location) {
+  function registerController($http, Auth, $location, Upload) {
     var rc = this;
 
     rc.Auth = Auth;
     rc.signup = function(){
-      $http
-        .post('/api/users', rc.signupUser)
+      Upload
+        .upload('/api/users', rc.signupUser)
         .then(function(returnData){
           console.log('SIGNUP : ',returnData );
           if (returnData.data._id) {
