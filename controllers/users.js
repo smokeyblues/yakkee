6a53c94aa47dbc1a0a80a24d85e6c494b99bb1bd
Aaron Pennington <aaron.l.pennington@gmail.com>
2017-01-21 16:26:56 -0700

    quick commit

--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,7 @@ module.exports = {
   create : (req, res)=>{
     // Creating registering a new user
     var file = req.files.files[0];
-    console.log(`file passed from create user: `, req.files.files[0]);
+    console.log(`file passed from create user: `, req);
 
     // fs.writeFileSync('/public/images/profile-pics', file, function(err){
     //   if (err) {
