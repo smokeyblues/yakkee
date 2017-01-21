b2e54b65f308c53fa449ec8bdb52faf7ef67aed3
Aaron Pennington <aaron.l.pennington@gmail.com>
2017-01-20 17:43:12 -0700

    quick commit

--- public/js/services/service.socket.js
+++ public/js/services/service.socket.js
@@ -17,12 +17,12 @@ angular.module('Yakkee').service('Socket', ['Auth', '$timeout',
         })
 
     };
-    sock.connect();
+    this.connect();
 
     // Wrap the Socket.io 'on' method
-    sock.on = function (eventName, callback) {
-      if (sock.socket) {
-        sock.socket.on(eventName, function (data) {
+    this.on = function (eventName, callback) {
+      if (this.socket) {
+        this.socket.on(eventName, function (data) {
           $timeout(function () {
             callback(data);
           });
@@ -31,16 +31,16 @@ angular.module('Yakkee').service('Socket', ['Auth', '$timeout',
     };
 
     // Wrap the Socket.io 'emit' method
-    sock.emit = function (eventName, data) {
-      if (sock.socket) {
-        sock.socket.emit(eventName, data);
+    this.emit = function (eventName, data) {
+      if (this.socket) {
+        this.socket.emit(eventName, data);
       }
     };
 
     // Wrap the Socket.io 'removeListener' method
-    sock.removeListener = function (eventName) {
-      if (sock.socket) {
-        sock.socket.removeListener(eventName);
+    this.removeListener = function (eventName) {
+      if (this.socket) {
+        this.socket.removeListener(eventName);
       }
     };
   }
